d09963bffc40b67b67b452d03c2ba1bb7852dfa4
Ben Doggett <bdoggett@gmail.com>
2025-07-11 14:42:56 -0400

    Configure frontend for typescript. Add Prisma/Postresql to backend.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Git Story (TypeScript + Vite + React)</h1>
+      {/* Add your UI here */}
+    </div>
+  );
+};
+
+export default App;
