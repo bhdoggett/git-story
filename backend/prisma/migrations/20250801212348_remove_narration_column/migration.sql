452e51304615a1a2ff0f1d8d8d1fb9ef230a3c8c
Ben Doggett <bdoggett@gmail.com>
2025-08-01 17:25:36 -0400

    Remove narration columnn from db schema

---
+++ backend/prisma/migrations/20250801212348_remove_narration_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `narration` on the `Repo` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Repo" DROP COLUMN "narration";
